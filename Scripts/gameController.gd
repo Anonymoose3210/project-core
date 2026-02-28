45d33b2fb548cf31c53bdec5288f1e6cdf2f00d1
Anonymoose3210 <saifg3210@gmail.com>
2026-02-28 13:49:00 -0600

    New nodes and World boundary
    Added new nodes with varying score rewards.
    Added world border that resets game when collided with.

--- Scripts/gameController.gd
+++ Scripts/gameController.gd
@@ -5,3 +5,7 @@ var total_points = 0
 func item_collected(item_value: int):
 	total_points += item_value
 	EventController.emit_signal("item_collected", total_points)
+
+func game_ended():
+	total_points = 0
+	get_tree().reload_current_scene()
--- Scripts/rock.gd
+++ Scripts/rock.gd
@@ -2,4 +2,4 @@ extends Node2D
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
-		get_tree().reload_current_scene()
+		GameController.game_ended()
