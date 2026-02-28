e4bbe29e1209cd51def7b8e70b57aa02581c1d5b
Anonymoose3210 <saifg3210@gmail.com>
2026-02-28 13:55:51 -0600

    High Score
    Added a high score variable that stores the highest score received by the player.
    Still need to implement a visual display.

--- Scripts/gameController.gd
+++ Scripts/gameController.gd
@@ -1,11 +1,14 @@
 extends Node
 
 var total_points = 0
+var high_score = 0
 
 func item_collected(item_value: int):
 	total_points += item_value
 	EventController.emit_signal("item_collected", total_points)
 
 func game_ended():
+	if total_points > high_score:
+		high_score = total_points
 	total_points = 0
 	get_tree().reload_current_scene()
